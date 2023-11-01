3af322a6107a4fb5d3f23674eb819eee78fe6c97
Mdfurqan29 <mdfurqanahmed14@gmail.com>
2023-11-01 14:44:46 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [inputValue , setInputValue] = useState('')
+const [TodoData,setTodoData] = useState([])
+
+
+  const add = ()=>{
+    if(!inputValue){
+      alert("enter data")
+    }else{
+setTodoData((pre)=>[...pre,inputValue])
+setInputValue('')
+}
+  }
+  const inputFun = (e)=>{
+    setInputValue(e.target.value)
+  }
+const edit = (i)=>{
+  let user = prompt("Enter New Data",TodoData[i])
+  TodoData[i] = user
+  setTodoData((pre)=>[...pre])
+}
+const del = (index)=>{
+  let a = TodoData.filter((e,i)=>i!==index)
+setTodoData(a)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='inpBtnMain'>
+      <h1>Todo List App</h1>
+
+    <input type='text' value={inputValue} onChange={inputFun}></input>
+    <button onClick={add}>Add</button>
+    </div>
+    <ul>
+      {
+TodoData.map((e,i)=><li key={i}>{e}<button className='btn' onClick={()=>edit(i)}>Edit</button><button className='btns' onClick={()=>del(i)}>Delete</button></li>)
+      }
+    </ul>
     </div>
   );
 }
